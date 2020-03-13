e3b274ae94248f22a09386aba86fc535390e10e5
Angel Cruz <37281529+angelcruzalv@users.noreply.github.com>
2020-03-12 23:48:17 -0600

    Entrada/Salida Screen completed
    Completada la interfaz importada en estas dos vistas. Se agregó nuevo diseño.

--- lib/src/screens/entrada_screen.dart
+++ lib/src/screens/entrada_screen.dart
@@ -2,139 +2,215 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/src/services/authentication.dart';
 import 'package:flutter_app/src/services/entrada_services.dart';
 
-
 class EntradaScreen extends StatefulWidget {
   @override
   _EntradaScreen createState() => new _EntradaScreen();
- }
+}
+
 class _EntradaScreen extends State<EntradaScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor:  Color.fromRGBO(0, 191, 166, 1.0),
-      ),
-        
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+
+
+        
         body: Container(
-          
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: <Color>[
-            //Color(0xFF78ffd6),
-            Color(0xff0079991),
-            Color(0xff0079991),
-            Color(0xff007991),
-            Color(0xff007991),
-            Color(0xff0079991),
-            Color(0xff0079991),
-            // Color(0xFF78ffd6),
-          ],
-        )),
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, left: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: 125.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Entradas',
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25.0),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 125.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.search),
-                          color: Colors.white,
-                          onPressed: () {},
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height - 50,
-              width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35.0),
-                      topRight: Radius.circular(35.0))),
-              child: ListView(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                children: <Widget>[
-                  
-                ],
+                  gradient: LinearGradient(
+                     colors: <Color>[
+                  //Color(0xFF78ffd6),
+                  Color(0xff0079991),
+                  Color(0xff0079991),
+                  Color(0xff007991),
+                  Color(0xff007991),
+                  Color(0xff0079991),
+                  Color(0xff0079991),
+                  // Color(0xFF78ffd6),
+                       ],
+                   )
+                ),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, left: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: 125.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Entradas',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                     
+                      width: 125.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.search),
+                            color: Colors.white,
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height - 50,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35.0),
+                        topRight: Radius.circular(35.0))),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+
+                  child: Column(
+                    children: <Widget>[
+                      StreamBuilder(
+                        stream: EntradaService().getEntradasStream(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            var entradas = snapshot.data.documents;
+                            List<EntradaItem> entradaItems = [];
+                            for (var entrada in entradas) {
+                              final entradaDpto = entrada.data['departamento'];
+                              final entradaDesc = entrada.data['descripcion'];
+                              final entradaFecha =
+                                  entrada.data['fecha'].toString();
+                              final entradaValor =
+                                  entrada.data['valor'].toString();
+                              entradaItems.add(EntradaItem(
+                                departamento: entradaDpto,
+                                descripcion: entradaDesc,
+                                fecha: entradaFecha,
+                                valor: entradaValor,
+                              ));
+                            }
+                            return Flexible(
+                              child: MaterialApp(
+                                home: Card(
+                                  child: ListView(
+                                    children: entradaItems,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                        }
+                      )
+                    ],
+
+                  ),
+                 //  padding: EdgeInsets.symmetric(vertical: 10.0),
+               
+                    
+                    // StreamBuilder
+                    // padding: EdgeInsets.symmetric(vertical: 10.0),
+                    // StreamBuilder(
+                    //     stream: EntradaService().getEntradasStream(),
+                    //     builder: (context, snapshot) {
+                    //       if (snapshot.hasData) {
+                    //         var entradas = snapshot.data.documents;
+                    //         List<EntradaItem> entradaItems = [];
+                    //         for (var entrada in entradas) {
+                    //           final entradaDpto = entrada.data['departamento'];
+                    //           final entradaDesc = entrada.data['descripcion'];
+                    //           final entradaFecha =
+                    //               entrada.data['fecha'].toString();
+                    //           final entradaValor =
+                    //               entrada.data['valor'].toString();
+                    //           entradaItems.add(EntradaItem(
+                    //             departamento: entradaDpto,
+                    //             descripcion: entradaDesc,
+                    //             fecha: entradaFecha,
+                    //             valor: entradaValor,
+                    //           ));
+                    //         }
+                    //         return Flexible(
+                    //           child: MaterialApp(
+                    //             home: Card(
+                    //               child: ListView(
+                    //                 children: entradaItems,
+                    //               ),
+                    //             ),
+                    //           ),
+                    //         );
+                    //       }
+                    //     }
+                    //   )
+                    
+                
+                )
+              )
+            ],
+          ),
         ),
+        //   body: Container(
+        //   children: <Widget>[
+        //     StreamBuilder(
+        //       stream: EntradaService().getEntradasStream(),
+        //       builder: (context, snapshot){
+        //         if(snapshot.hasData){
+        //           var entradas = snapshot.data.documents;
+        //           List<EntradaItem> entradaItems = [];
+        //           for (var entrada in entradas){
+        //             final entradaDpto = entrada.data['departamento'];
+        //             final entradaDesc = entrada.data['descripcion'];
+        //             final entradaFecha = entrada.data['fecha'].toString();
+        //             final entradaValor = entrada.data['valor'].toString();
+        //             entradaItems.add(EntradaItem(
+        //               departamento: entradaDpto,
+        //               descripcion: entradaDesc,
+        //               fecha: entradaFecha ,
+        //               valor: entradaValor,
+        //             ));
+        //           }
+        //           return Flexible(
+        //             child: MaterialApp(
+        //               home: Card(
+        //                 child: ListView(
+        //                   children: entradaItems,
+        //                 ),
+        //               ),
+        //             ),
+        //           );
+        //         }
+        //       }
+        //     )
+        //   ],
+        // ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'addEntrada');
+          },
+          child: Icon(
+            Icons.add,
+          ),
+          backgroundColor: Color.fromRGBO(192, 0, 25, 1.0),
         ),
-      //   body: Container(
-      //   children: <Widget>[
-      //     StreamBuilder(
-      //       stream: EntradaService().getEntradasStream(),
-      //       builder: (context, snapshot){
-      //         if(snapshot.hasData){
-      //           var entradas = snapshot.data.documents;
-      //           List<EntradaItem> entradaItems = [];
-      //           for (var entrada in entradas){
-      //             final entradaDpto = entrada.data['departamento'];
-      //             final entradaDesc = entrada.data['descripcion'];
-      //             final entradaFecha = entrada.data['fecha'].toString();
-      //             final entradaValor = entrada.data['valor'].toString();
-      //             entradaItems.add(EntradaItem(
-      //               departamento: entradaDpto,
-      //               descripcion: entradaDesc,
-      //               fecha: entradaFecha , 
-      //               valor: entradaValor,
-      //             ));
-      //           }
-      //           return Flexible(
-      //             child: MaterialApp(
-      //               home: Card(
-      //                 child: ListView(
-      //                   children: entradaItems,
-      //                 ),
-      //               ),
-      //             ),
-      //           );
-      //         }
-      //       }
-      //     )
-      //   ],
-      // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, 'addEntrada');
-        },
-        child: Icon(Icons.add,),
-        backgroundColor:  Color.fromRGBO(192, 0, 25, 1.0),
-        ),
-       ),
+      ),
     );
-   
   }
 }
 
@@ -145,35 +221,48 @@ class EntradaItem extends StatelessWidget {
   final String valor;
 
   EntradaItem({this.departamento, this.descripcion, this.fecha, this.valor});
- @override
- Widget build(BuildContext context) {
-
-   return Container(
+  @override
+  Widget build(BuildContext context) {
+    return Container(
         padding: EdgeInsets.only(right: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.transparent
+        ),
         child: Card(
           color: Colors.transparent,
           elevation: 0.0,
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Icon(Icons.account_balance_wallet, color: Colors.green,),
-                title: Text('$departamento'),
-                subtitle:
-                    Text('$descripcion'),
-                trailing: Text(fecha[0]+fecha[1]+fecha[2]+fecha[3]+ fecha[4]
-                 +'\n'+fecha[6]+fecha[7]+fecha[8]+fecha[9]
-                 ,style: TextStyle(fontSize: 10.0),)               
-              ),
+                  leading: Icon(
+                    Icons.account_balance_wallet,
+                    color: Colors.green,
+                  ),
+                  title: Text('$departamento'),
+                  subtitle: Text('$descripcion'),
+                  trailing: Text(
+                    fecha[0] +
+                        fecha[1] +
+                        fecha[2] +
+                        fecha[3] +
+                        fecha[4] +
+                        '\n' +
+                        fecha[6] +
+                        fecha[7] +
+                        fecha[8] +
+                        fecha[9],
+                    style: TextStyle(fontSize: 10.0),
+                  )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Text('$valor', style: TextStyle(fontSize: 22.0, color: Colors.green ))
+                  Text('$valor',
+                      style: TextStyle(fontSize: 22.0, color: Colors.green))
                 ],
               )
             ],
           ),
-        )
-      );
+        ));
 //   return new ListTile(
 //     leading: CircleAvatar(
 //       child: Text(departamento[0], style: TextStyle(color: Colors.white),),
@@ -194,5 +283,5 @@ class EntradaItem extends StatelessWidget {
 //         ,style: TextStyle(fontSize: 10.0),),
 //   onTap: (){},
 //  );
- }
+  }
 }
--- lib/src/screens/salida_screen.dart
+++ lib/src/screens/salida_screen.dart
@@ -27,31 +27,101 @@ class _SalidaScreenState extends State<SalidaScreen> {
 
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
         theme: ThemeData(
         primaryColor:  Color.fromRGBO(0, 191, 166, 1.0),
       ),
-        home: new Scaffold(
-            appBar: AppBar(
-              title: const Text('Salidas', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0)),),
-              automaticallyImplyLeading: false,
-              actions: <Widget>[
-                ButtonBar(
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text(
-                        "Salir", style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0)),                      ),
-                      onPressed: () {
-                        Authentication().signOut();
-                        Navigator.pushNamed(context, "");
-                      },
+       home: new Scaffold(
+            // appBar: AppBar(
+            //   title: const Text('Salidas', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0)),),
+            //   automaticallyImplyLeading: false,
+            //   actions: <Widget>[
+            //     ButtonBar(
+            //       children: <Widget>[
+            //         FlatButton(
+            //           child: Text(
+            //             "Salir", style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0)),                      ),
+            //           onPressed: () {
+            //             Authentication().signOut();
+            //             Navigator.pushNamed(context, "");
+            //           },
+            //         )
+            //       ],
+            //     ),
+            //   ],
+            // ),
+            body: Container(
+                decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            //Color(0xFF78ffd6),
+                            Color(0xff0079991),
+                            Color(0xff0079991),
+                            Color(0xff007991),
+                            Color(0xff007991),
+                            Color(0xff0079991),
+                            Color(0xff0079991),
+                        // Color(0xFF78ffd6),
+                      ],
                     )
-                  ],
                 ),
-              ],
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, left: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Salidas',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          color: Colors.white,
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-            body: Column(
-              children: <Widget>[
-                StreamBuilder(
+            SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height - 50,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35.0),
+                      topRight: Radius.circular(35.0))),
+              child: Container(
+                child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Column(     
+                   children: <Widget>[
+
+                  StreamBuilder(
                     stream: SalidaService().getSalidasStream(),
                     builder: (context, snapshot) {
                       if (snapshot.hasData) {
@@ -91,8 +161,57 @@ class _SalidaScreenState extends State<SalidaScreen> {
                       }
                     }
                   ),
-              ],
-            ),
+                    
+                      ],
+              ),  )
+            ),)
+          ],
+        ),
+      ),
+            // body: Column(
+            //   children: <Widget>[
+            //     StreamBuilder(
+            //         stream: SalidaService().getSalidasStream(),
+            //         builder: (context, snapshot) {
+            //           if (snapshot.hasData) {
+            //             var salidas = snapshot.data.documents;
+            //             //List<Text> salidaWidgets = [];
+            //             List<SalidaItem> salidaItems = [];
+            //             List<String> salidaItemsString = [];
+            //             for (var salida in salidas) {
+            //               final salidaDpto = salida.data["departamento"].toString();
+            //               final salidaDesc = salida.data["descripcion"].toString();
+            //               final salidaFecha = salida.data["fecha"].toString();
+            //               final salidaTotal = salida.data["total"].toString();
+            //               final salidaValor = salida.data["valor"].toString();
+            //               //salidaWidgets.add(Text('$salidaDpto , Descripción: $salidaDesc , Fecha: $salidaFecha , Total: $salidaTotal , Valor: $salidaValor'));
+            //               salidaItems.add(SalidaItem(
+            //                 departamento: salidaDpto,
+            //                 descripcion: salidaDesc,
+            //                 valor: salidaValor,
+            //                 total: salidaTotal,
+            //                 fecha: salidaFecha,
+            //               ));
+            //               salidaItemsString.add(salidaFecha);
+                          
+            //             }
+            //             salidaItemsString.sort((a, b) => a.compareTo(b));
+            //             print('Ordenado: $salidaItemsString');
+            //             return Flexible(
+            //                 child: MaterialApp(                                                         
+            //                     //theme: ThemeData(primaryColor: Colors.red),
+            //                     home: Card(
+            //                       child: ListView(
+            //                         children: salidaItems,
+            //                       ),
+            //                     )
+            //                   )
+            //                );
+            //           }
+            //         }
+            //       ),
+            //   ],
+            // ),
             floatingActionButton: FloatingActionButton(
               child: Icon(Icons.add),
               backgroundColor: Color.fromRGBO(192, 0, 25, 1.0),
@@ -119,30 +238,31 @@ class SalidaItem extends StatelessWidget {
       this.fecha});
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(departamento[0], style: TextStyle(color: Colors.white),),
-        backgroundColor: Color.fromRGBO(0, 121, 191, 1.0),
-      ),
-      title: Text('$departamento', style: TextStyle(fontWeight: FontWeight.bold)),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('$descripcion'),
-          Text('Valor: $valor\nTotal: $total',
-            style: TextStyle(fontWeight: FontWeight.bold),
+    return  Container(
+        padding: EdgeInsets.only(right: 20.0),
+        child: Card(
+          color: Colors.transparent,
+          elevation: 0.0,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.account_balance_wallet, color: Colors.red,),
+                title: Text('$departamento'),
+                subtitle:
+                    Text('$descripcion'),
+                trailing: Text(fecha[0]+fecha[1]+fecha[2]+fecha[3]+ fecha[4]
+                 +'\n'+fecha[6]+fecha[7]+fecha[8]+fecha[9]
+                 ,style: TextStyle(fontSize: 10.0),)               
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text('$valor', style: TextStyle(fontSize: 22.0, color: Colors.red ))
+                ],
+              )
+            ],
           ),
-        ],
-      ),
-      isThreeLine: true,
-      contentPadding: EdgeInsets.all(10.0),
-      trailing: Text(
-        fecha[0]+fecha[1]+fecha[2]+fecha[3]+ fecha[4]
-        +'\n'+fecha[6]+fecha[7]+fecha[8]+fecha[9]
-        ,style: TextStyle(fontSize: 10.0),),
-      onTap: () {
-     
-      },
-    );
+        )
+      );   
   }
 }
